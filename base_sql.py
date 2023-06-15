34e5122936d72d9a62ae1963d0aed290af5e99b0
phongvu2010 <phongvu2010@gmail.com>
2023-06-15 23:39:28 +0700

    master

--- base_sql.py
+++ base_sql.py
@@ -6,46 +6,31 @@ from sqlalchemy.orm import sessionmaker
 from pprint import pprint
 from dotenv import load_dotenv
 
+# Take environment variables from .env
 load_dotenv()
-
 db = os.environ.get('DB')
-print(db)
-# check if there is an database string in the .env file
-# if db:
-#     db = db
-# else:
-#     # if no variable is specified, use the default string below
-#     db = 'postgresql+psycopg2://test:testpassword@localhost:5432/price_data'
 
-# engine = create_engine(db)
+engine = create_engine(db)
 # engine.connect()
+# pprint(f'Connection successful! : {engine}')
 
-# pprint(f'connection successful! : {engine}')
-# # create a session variable. Allows all our transactions to be ran in the context of a session
-# Session = sessionmaker(bind = engine)
+# Create a session variable. Allows all our transactions to be ran in the context of a session
+Session = sessionmaker(bind = engine)
 
-# Base = declarative_base()
+Base = declarative_base()
 
-
-
-
-
-
-
-# # database_insert.py
-# from base_sql import engine, Base, Session
-
-# def create_table():
-#     # create a new session
-#     session = Session()
-#     # attempts to create the base tables and populate the db in a session. in case of an issue rolls back the session
-#     try:
-#         # Generate schema
-#         Base.metadata.create_all(engine)
-#         session.commit()
-#         print(f"connected & db populated")
-#     except Exception as e:
-#         session.rollback()
-#         print(e)
-
-#     session.close()
+def create_table():
+    # Create a new session
+    session = Session()
+    # Attempts to create the base tables and populate the db in a session.
+    # In case of an issue rolls back the session
+    try:
+        # Generate schema
+        Base.metadata.create_all(engine)
+        session.commit()
+        print(f'Connected & db populated')
+    except Exception as e:
+        session.rollback()
+        print(e)
+    finally:
+        session.close()
--- bot.py
+++ bot.py
@@ -1,58 +1,51 @@
-import os
+# import os
 import websocket as wb
 from pprint import pprint
-import json
+# import json
 
-from binance.client import Client
-from binance.enums import *
-from dotenv import load_dotenv      # pip3 install --upgrade
-
-# load environment variables
-load_dotenv()
-
-# create client
-client = Client(os.getenv('BINANCE_API_KEY'), os.getenv('BINANCE_API_SECRET'))
-
-
-
-
+from base_sql import Session, create_table
+# from binance.client import Client
+# from binance.enums import *
 from datetime import datetime
-from database_insert import create_table
-from base_sql import Session
+from dotenv import load_dotenv
 from models import CryptoPrice
 
-# this functions creates the table if it does not exist
+# This functions creates the table if it does not exist
 create_table()
 
-# create a new session
+# Load environment variables
+load_dotenv()
+
+# Create client
+# API_KEY = os.environ.get('API_KEY')
+# API_SECRET = os.environ.get('API_SECRET')
+# client = Client(API_KEY, API_SECRET)
+# client = Client()
+
+# Create a new session
 session = Session()
 
-BINANCE_SOCKET = 'wss://stream.binance.com:9443/stream?streams=ethusdt@kline_1m/btcusdt@kline_1m'
+# BINANCE_SOCKET = 'wss://stream.binance.com:9443/stream?streams=ethusdt@kline_1m/btcusdt@kline_1m'
+BINANCE_SOCKET = 'wss://stream.binance.com:9443/btcusdt@kline_1m'
 closed_prices = []
 
+# def on_open(ws):
+#     print('Connection opened')
 
-API_KEY = os.environ.get("API_KEY")
-API_SECRET = os.environ.get("API_SECRET")
-client = Client(API_KEY, API_SECRET, tld='us')
+# def on_close(ws):
+#     print('Closed connection')
 
-def on_open(ws):
-    print("connection opened")
-
-
-def on_close(ws):
-    print("closed connection")
-
-
-def on_error(ws, error):
-    print(error)
-
+# def on_error(ws, error):
+#     print(error)
 
 def on_message(ws, message):
-    message = json.loads(message)
+    # message = json.loads(message)
     pprint(message)
+
     candle = message['k']
     trade_symbol = message['s']
     is_candle_closed = candle['x']
+
     global closed_prices
     if is_candle_closed:
         symbol = candle['s']
@@ -61,22 +54,31 @@ def on_message(ws, message):
         high = candle['h']
         low = candle['l']
         volume = candle['v']
-        pprint(f"closed: {closed}")
-        pprint(f"open: {open}")
-        pprint(f"high: {high}")
-        pprint(f"low: {low}")
-        pprint(f"volume: {volume}")
+
+        pprint(f'closed: {closed}')
+        pprint(f'open: {open}')
+        pprint(f'high: {high}')
+        pprint(f'low: {low}')
+        pprint(f'volume: {volume}')
+
         closed_prices.append(float(closed))
-        # create price entries
-        crypto = CryptoPrice(crypto_name=symbol, open_price=open, close_price=closed,
-                             high_price=high, low_price=low, volume=volume, time=datetime.utcnow())
+
+        # Create price entries
+        crypto = CryptoPrice(crypto_name = symbol, open_price = open, close_price = closed,
+                             high_price = high, low_price = low, volume = volume, time = datetime.utcnow())
+
         try:
             session.add(crypto)
             session.commit()
         except Exception as e:
             session.rollback()
             print(e)
-        session.close()
+        finally:
+            session.close()
 
-ws = wb.WebSocketApp(BINANCE_SOCKET, on_open=on_open, on_close=on_close, on_error=on_error, on_message=on_message)
+ws = wb.WebSocketApp(BINANCE_SOCKET,
+                    #  on_open = on_open,
+                    #  on_close = on_close,
+                    #  on_error = on_error,
+                     on_message = on_message)
 ws.run_forever()
