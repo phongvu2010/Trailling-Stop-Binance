4c76fdb49d7c8b3d3a424c988f60bc54abbb70c7
phongvu2010 <phongvu2010@gmail.com>
2023-06-25 05:35:57 +0700

    master

--- dataset.py
+++ dataset.py
@@ -22,7 +22,7 @@ def get_klines(symbol, tick_interval = '5m'):
                          3: 'low', 4: 'close', 5: 'volume'}, inplace = True)
 
     df['start_time'] = df['start_time'].apply(lambda x: datetime.fromtimestamp(x / 1000))
-    df['start_time'] = pd.to_datetime(df['start_time'], utc = True)
+    df['start_time'] = pd.to_datetime(df['start_time'], utc = False)
 
     # df['start_time'] = df['start_time'].dt.tz_localize(tz = 'Asia/Ho_Chi_Minh')
 
